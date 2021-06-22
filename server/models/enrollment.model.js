01596260868ef7da04e1819c5d93d0b7869fa8cf
Jake <joregister91@gmail.com>
2021-06-22 18:24:04 -0400

    first commit

---
+++ server/models/enrollment.model.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose'
+
+const EnrollmentSchema = new mongoose.Schema({
+  course: {type: mongoose.Schema.ObjectId, ref: 'Course'},
+
+  student: {type: mongoose.Schema.ObjectId, ref: 'User'},
+
+  lessonStatus: [{
+    lesson: {type: mongoose.Schema.ObjectId, ref: 'Lesson'}, 
+    complete: Boolean}],
+
+  enrolled: {
+    type: Date,
+    default: Date.now
+  },
+
+  updated: Date,
+  completed: Date
+})
+
+export default mongoose.model('Enrollment', EnrollmentSchema)
